d23f6a38b09eb92a290e07619496ff0024913a37
Mohanapriya-Vadivel <144239583+Mohanapriya-Vadivel@users.noreply.github.com>
2025-10-02 13:54:51 +0530

    Create App.jsx

---
+++ day-21/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import UserPagination from "./UserPagination";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Hooks - Pagination Example</h1>
+      <UserPagination />
+    </div>
+  );
+}
+
+export default App;
