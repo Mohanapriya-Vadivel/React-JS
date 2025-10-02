f8661cc20803b443d294dbfb135714ef97af6c86
Mohanapriya-Vadivel <144239583+Mohanapriya-Vadivel@users.noreply.github.com>
2025-10-02 13:50:10 +0530

    Create App.jsx

---
+++ day-18/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import Users from "./Users";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Hooks - Fetch API Data</h1>
+      <Users />
+    </div>
+  );
+}
+
+export default App;
