38f971f376e6e5aada22ee05aa30a2c1894fe7fc
Mohanapriya-Vadivel <144239583+Mohanapriya-Vadivel@users.noreply.github.com>
2025-10-02 13:43:02 +0530

    Create App.jsx

---
+++ day-14/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import UserForm from './UserForm';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Mini Project – Day 14</h1>
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
