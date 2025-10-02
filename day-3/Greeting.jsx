325f9d4988ac2dfc0b29394748df5b48fb25626b
Mohanapriya-Vadivel <144239583+Mohanapriya-Vadivel@users.noreply.github.com>
2025-10-02 13:26:30 +0530

    Create Greeting.jsx

---
+++ day-3/Greeting.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+// Functional Component
+function Greeting(props) {
+  return (
+    <div>
+      <h2>Hello, {props.name}!</h2>
+      <p>Welcome to React learning journey! 🎉</p>
+    </div>
+  );
+}
+
+export default Greeting;
