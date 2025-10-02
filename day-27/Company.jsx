094a2977a4b41427eac47cc2edea69143dc9eb05
Mohanapriya-Vadivel <144239583+Mohanapriya-Vadivel@users.noreply.github.com>
2025-10-02 14:07:34 +0530

    Create Company.jsx

---
+++ day-27/Company.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Company() {
+  return (
+    <div>
+      <h3>Our Company</h3>
+      <p>About the company and its mission.</p>
+    </div>
+  );
+}
+
+export default Company;
