0b2ae5ed526eefe5b0f67e27195cd84c2e107e97
Mohanapriya-Vadivel <144239583+Mohanapriya-Vadivel@users.noreply.github.com>
2025-10-02 13:53:26 +0530

    Create App.jsx

---
+++ day-20/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import UserSearch from "./UserSearch";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Hooks - Search in API Data</h1>
+      <UserSearch />
+    </div>
+  );
+}
+
+export default App;
