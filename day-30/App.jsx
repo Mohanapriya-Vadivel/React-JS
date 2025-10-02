3914ec768d16e84df6527b6ede08f9f7e57ad4af
Mohanapriya-Vadivel <144239583+Mohanapriya-Vadivel@users.noreply.github.com>
2025-10-02 17:13:40 +0530

    Create App.jsx

---
+++ day-30/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import UserForm from "./UserForm";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Day 29 - React Forms</h1>
+      <UserForm />
+    </div>
+  );
+}
+
+export default App;
