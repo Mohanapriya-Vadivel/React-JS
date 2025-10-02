60ffac15b453c752f4d5d741c98dde4ae564f21d
Mohanapriya-Vadivel <144239583+Mohanapriya-Vadivel@users.noreply.github.com>
2025-10-02 13:24:21 +0530

    Create App.jsx

---
+++ day-1/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to My First React App! 😄</h1>
+      <p>This is my first React component.</p>
+    </div>
+  );
+}
+
+export default App;
