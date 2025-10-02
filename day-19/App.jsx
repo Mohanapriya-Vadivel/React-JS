18110be81d435f2c60ebba142f6115640ff75561
Mohanapriya-Vadivel <144239583+Mohanapriya-Vadivel@users.noreply.github.com>
2025-10-02 13:51:38 +0530

    Create App.jsx

---
+++ day-19/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import UsersWithErrorHandling from "./UsersWithErrorHandling";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Hooks - API Error Handling</h1>
+      <UsersWithErrorHandling />
+    </div>
+  );
+}
+
+export default App;
