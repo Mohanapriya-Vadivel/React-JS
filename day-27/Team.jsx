158ed6f94551ac1fd01070ca53079cec701e4a91
Mohanapriya-Vadivel <144239583+Mohanapriya-Vadivel@users.noreply.github.com>
2025-10-02 14:07:15 +0530

    Create Team.jsx

---
+++ day-27/Team.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Team() {
+  return (
+    <div>
+      <h3>Our Team</h3>
+      <p>Meet our amazing team members!</p>
+    </div>
+  );
+}
+
+export default Team;
